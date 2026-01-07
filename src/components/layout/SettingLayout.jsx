0eff829d595b6d3ffaebef907e23084efad4fe0e
sohailamir06 <amir.sohail@leanport.info>
2026-01-07 12:18:59 +0530

    adding Reset password

--- src/components/layout/SettingLayout.jsx
+++ src/components/layout/SettingLayout.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Settings from "../../pages/admin/Settings";
+import ResetPassword from "../settings/ResetPassword";
+
 function SettingLayout() {
   return (
     <>
       <Settings />
       <Outlet />
+      <div className="mt-6">
+        <ResetPassword />
+      </div>
     </>
   );
 }
